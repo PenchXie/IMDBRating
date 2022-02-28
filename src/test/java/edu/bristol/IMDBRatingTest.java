0c16cc08281b6723b11297408a1b96cbd648f584
Pench Xie <rm21322@bristol.ac.uk>
2022-02-28 17:32:08 +0000

    Add tests and optimize codes

--- src/test/java/edu/bristol/IMDBRatingTest.java
+++ src/test/java/edu/bristol/IMDBRatingTest.java
@@ -11,10 +11,16 @@ public class IMDBRatingTest
         float averageRating;
         IMDBRating rater = new IMDBRating();
 
-        averageRating = rater.addNewRating(2);
-        assertTrue(averageRating == 2.0, "Adding 1st rating: average should be 2.0");
+        averageRating = rater.addNewRating(0);
+        assertTrue(averageRating == 0.0, "Adding 1st rating: average should be 2.0");
 
-        averageRating = rater.addNewRating(4);
-        assertTrue(averageRating == 3.0, "Adding 2nd rating: average should be 3.0");
+        averageRating = rater.addNewRating(2);
+        assertTrue(averageRating == 1.0, "Adding 1st rating: average should be 2.0");
+
+        averageRating = rater.addNewRating(10);
+        assertTrue(averageRating == 4.0, "Adding 2nd rating: average should be 3.0");
+
+        averageRating = rater.addNewRating(-5);
+        assertTrue(averageRating == 4.0, "Adding 3rd rating: average should be 3.0 as well");
     }
 }
