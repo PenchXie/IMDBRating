b11e7217d8b86fe82433556a00807e69daa7463a
Pench Xie <rm21322@bristol.ac.uk>
2022-02-28 22:27:52 +0000

    try to fix bug 1st

--- src/test/java/edu/bristol/IMDBRatingTest.java
+++ src/test/java/edu/bristol/IMDBRatingTest.java
@@ -23,7 +23,7 @@ public class IMDBRatingTest
         averageRating = rater.addNewRating(-5);
         assertTrue(averageRating == 4.0, "Adding 3rd rating: average should be 3.0 as well");
 
-        averageRating = rater.addNewRating(9);
+        averageRating = rater.addNewRating(11);
         assertTrue(averageRating == 4.0, "Adding 3rd rating: average should be 3.0 as well");
     }
 }
